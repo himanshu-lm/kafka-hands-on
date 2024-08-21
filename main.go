80f0f94ae6cd1532359a936f4977ab2363dce4d5
himanshu.balurkar <himanshu.balurkar@logicmonitor.com>
2024-08-21 14:53:08 +0530

    added instructions for executing the code

--- main.go
+++ main.go
@@ -25,4 +25,19 @@ This application will have a single kafka broker, one kafka producer and one kaf
 Randomly messages will be created and sent to the broker.
 single topic : mytopic1
 broker address : localhost:9092
+
+
+***************RUN THE APPLICATION******************
+1. open terminal and start zookeeper : run the below cmd
+/usr/local/opt/kafka/bin/zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
+
+2. open another terminal and create a kafka topic or whatever stuff you want
+then start the kafka server
+/usr/local/opt/kafka/bin/kafka-server-start /usr/local/etc/kafka/server.properties
+
+3. go run main.go
+
+for more reference
+https://medium.com/@bectorhimanshu/setting-up-installing-and-starting-the-kafka-server-on-macos-7d505e32039a
+
 */
